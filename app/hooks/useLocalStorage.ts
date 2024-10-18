8ceeb2c86d8df02322490fce174ecff47b501bd3
dev1 <dev1@teewan.xyz>
2024-10-18 11:35:04 +0800

    feat: update local storage hook

--- app/hooks/useLocalStorage.ts
+++ app/hooks/useLocalStorage.ts
@@ -1,12 +1,55 @@
-export const useLocalStorage = (key: string) => {
-  const getLocalStorage = () => {
-    const item = localStorage.getItem(key);
-    return item ? JSON.parse(item) : {};
-  };
+import {
+  type Dispatch,
+  type SetStateAction,
+  useEffect,
+  useRef,
+  useState,
+} from "react";
+import { latestVersion, updateToLatestVersion } from "../utils/Versioning";
 
-  const setLocalStorage = (value: any) => {
-    localStorage.setItem(key, JSON.stringify(value));
-  };
+export function useLocalStorage<T>(
+  key: string,
+  defaultState: T
+): [T, Dispatch<SetStateAction<T>>] {
+  const state = useState<T>(() => {
+    try {
+      const value = localStorage.getItem(key);
+      if (value) {
+        const parsedValue = JSON.parse(value);
+        return updateToLatestVersion(parsedValue) as T;
+      }
+    } catch (error: any) {
+      if (typeof window !== "undefined") {
+        console.error(error);
+      }
+    }
 
-  return [getLocalStorage, setLocalStorage] as const;
-};
+    return defaultState;
+  });
+  const value = state[0];
+
+  const isFirstRenderRef = useRef(true);
+  useEffect(() => {
+    if (isFirstRenderRef.current) {
+      isFirstRenderRef.current = false;
+      return;
+    }
+    try {
+      if (value === null) {
+        localStorage.removeItem(key);
+      } else {
+        const valueWithVersion = {
+          version: latestVersion, // Add the version back here
+          ...value, // Retain the existing data
+        };
+        localStorage.setItem(key, JSON.stringify(valueWithVersion));
+      }
+    } catch (error: any) {
+      if (typeof window !== "undefined") {
+        console.error(error);
+      }
+    }
+  }, [value, key]);
+
+  return state;
+}
